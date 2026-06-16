36dcedb95ce2b888ccae6d0f25b6f9fc52f0ace5
shub77 <80330717@qq.com>
2026-06-16 13:21:12 +0800

    feat: 合并当前和历史开课查询页面
    - 创建UnifiedSemesterQuery.vue统一组件
    - 默认显示当前学期，支持选择任意学期
    - 自动计算当前学期（8月-1月为秋季，2月-7月为春季）
    - 移除独立的"历史开课查询"菜单项
    - 旧路由/query/historical-semester重定向到/query/semester
    - 更新导航菜单，简化为用户友好的"开课查询"
    
    版本: v1.5.7

--- client/src/components/Layout.vue
+++ client/src/components/Layout.vue
@@ -71,8 +71,7 @@
             <span>查询报表</span>
           </template>
           <el-menu-item index="/query/plan">培养方案查询</el-menu-item>
-          <el-menu-item index="/query/semester">当前开课查询</el-menu-item>
-          <el-menu-item index="/query/historical-semester">历史开课查询</el-menu-item>
+          <el-menu-item index="/query/semester">开课查询</el-menu-item>
           <el-menu-item index="/query/textbook">教材使用查询</el-menu-item>
           <el-menu-item index="/query/historical-textbook">历史教材查询</el-menu-item>
         </el-sub-menu>
